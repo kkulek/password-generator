11190a809378eb92de78a62d1195ca14753ff687
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-04 19:54:38 +0100

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'darkest': '#18171F',
+                'dark': '#24232C',
+                'light': '#817D92',
+                'lightest': '#E6E5EA',
+                'accent': '#A4FFAF',
+                'pass': {
+                    weakest: '#F64A4A',
+                    weak: '#FB7C58',
+                    medium: '#F8CD65',
+                    strong: '#A4FFAF'
+                }
+            }
+        },
+    },
+    plugins: [],
 }
